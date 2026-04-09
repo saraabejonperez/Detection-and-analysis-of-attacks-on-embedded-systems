cdbfd2314940890e711508e991d33c958dd69a40
saraabejonperez <saraabejonperez@gmail.com>
2026-04-09 09:33:24 +0200

    Página web base sin funcionalidad. Ref #17

--- dos_detector/web_app/app/__init__.py
+++ dos_detector/web_app/app/__init__.py
@@ -6,8 +6,13 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
+    # API routes
     from .routes.health import health_bp
 
-    app.register_blueprint(health_bp)
+    # Pages (frontend)
+    from .routes.pages.dashboard import dashboard_bp
+
+    app.register_blueprint(health_bp, url_prefix="/api")
+    app.register_blueprint(dashboard_bp)
 
     return app
